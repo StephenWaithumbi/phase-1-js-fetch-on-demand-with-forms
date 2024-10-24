63e2e5eef11e197cba16ab87a7166112534e721d
Stephen Waithumbi <stephenwaithumbi14@gmail.com>
2024-10-25 00:28:04 +0300

    Hurayy

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 const init = () => {
+    const inputForm = document.querySelector("form");
+    inputForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const input = document.querySelector("#searchByID")
+
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(res => res.json())
+        .then(data => {
+            const movieTitle = document.getElementById("title")
+            const p = document.getElementById('summary')
+
+            movieTitle.innerText = data.title;
+            p.innerText = data.summary;
+
+        });
+
+        inputForm.reset();
+    });
   
 }
 
 document.addEventListener('DOMContentLoaded', init);
+
